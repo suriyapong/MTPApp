3fb8bf44ebaf46594260add35ed53125b51c405a
suriyapong <suriyapong_s@hotmail.com>
2019-01-29 17:40:45 +0700

    Calculate

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
+using MTPLibrary;
+
 namespace TestGit
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sum");
+            Console.ReadLine();
+
+            Calculator servicec = new Calculator();
+
+            //Console.WriteLine(servicec.Add(1, 2));
+            //Console.WriteLine();
+
+            Console.Write("Please insert input 1 : ");
+            string x = Console.ReadLine();
+
+            Console.Write("Please insert input 2 : ");
+            string y = Console.ReadLine();
+
+            int s1 = Convert.ToInt32(x);
+            int s2 = Convert.ToInt32(y);
+
+            int sum = servicec.Add(s1,s2);
+
+            string msg = $"Your Ans : { x } +  { y } = {sum} ";
+
+            Console.WriteLine(msg);
+
+            Console.ReadLine();
+
         }
     }
 }
