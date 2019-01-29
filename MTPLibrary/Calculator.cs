b6cc792bf74d8bfedebfd1cb5de7553639aa150b
suriyapong <suriyapong_s@hotmail.com>
2019-01-29 19:18:21 +0700

    Cal

--- MTPLibrary/Calculator.cs
+++ MTPLibrary/Calculator.cs
@@ -6,9 +6,56 @@ namespace MTPLibrary
 {
     public class Calculator
     {
+        private int _x = 0;
+
+        private int _sum = 0;
+
+        private List<int> _list;
+
+        private Boolean _abs = true;
+
+        public Calculator(Boolean abs)
+        {
+            _abs = abs;
+            _list = new List<int>();
+        }
+
         public int Add(int a, int b)
         {
-           return a + b;
+            return a + b;
+        }
+
+        public string GetInput(int n)
+        {
+            if (_abs)
+            {
+                n = Math.Abs(n);
+            }
+            _x = n;
+
+            _list.Add(n);
+
+            string x = SumAll(_sum, _x).ToString();
+
+            return x;
+        }
+
+        private int SumAll(int x, int y)
+        {
+            _sum = x +y;
+            return _sum;
+        }
+
+        public string GetHistory()
+        {
+            string result = "";
+
+            foreach (var item in _list)
+            {
+                result += item + "+";
+            }
+
+            return result;
         }
     }
 }
--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using MTPLibrary;
 
 namespace TestGit
@@ -11,25 +11,69 @@ namespace TestGit
             Console.WriteLine("Sum");
             Console.ReadLine();
 
-            Calculator servicec = new Calculator();
+
 
             //Console.WriteLine(servicec.Add(1, 2));
             //Console.WriteLine();
 
-            Console.Write("Please insert input 1 : ");
-            string x = Console.ReadLine();
+            //Console.Write("Please insert input 1 : ");
+            //string x = Console.ReadLine();
 
-            Console.Write("Please insert input 2 : ");
-            string y = Console.ReadLine();
+            //Console.Write("Please insert input 2 : ");
+            //string y = Console.ReadLine();
 
-            int s1 = Convert.ToInt32(x);
-            int s2 = Convert.ToInt32(y);
+            //int s1 = Convert.ToInt32(x);
+            //int s2 = Convert.ToInt32(y);
 
-            int sum = servicec.Add(s1,s2);
+            //int sum = servicec.Add(s1,s2);
 
-            string msg = $"Your Ans : { x } +  { y } = {sum} ";
+            //string msg = $"Your Ans : { x } +  { y } = {sum} ";
+
+            Calculator servicec1 = new Calculator(true);
+
+            Calculator servicec2 = new Calculator(false);
+
+
+            string sum1 = "";
+            string sum2 = "";
+            int i = 0;
+
+            while (true)
+            {
+                Console.Write("Please insert input : ");
+                string n = Console.ReadLine();
+  
+                bool isNumeric = int.TryParse(n, out int mm);
+                if (isNumeric)
+                {
+                    if (i%2 == 0)
+                    {
+                        sum1 = servicec1.GetInput(Convert.ToInt32(mm));
+                    }
+                    else
+                    {
+                        sum2 = servicec2.GetInput(Convert.ToInt32(mm));
+                    }
+                   
+                }
+                else
+                {
+                    break;
+                }
+
+                i++;
+               
+            }
+
+
+            string set1 = String.Join(",", servicec1.GetHistory());
+
+            string set2 = String.Join(",", servicec2.GetHistory());
+
+            string msg = $"SUM คู่ { set1 } :  { sum1 } SUM คี่ { set2 } : { sum2 }";
 
             Console.WriteLine(msg);
+
 
             Console.ReadLine();
 
